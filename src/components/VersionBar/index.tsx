73b93dc23da6ebbe64469d056290616a6b87408c
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2021-04-30 00:45:30 +0530

    Update index.tsx

--- src/components/VersionBar/index.tsx
+++ src/components/VersionBar/index.tsx
@@ -25,17 +25,17 @@ const VersionBar = () => {
         Version:
       </Text>
       <ButtonMenu variant="primary" scale="sm" activeIndex={1}>
-        <ButtonMenuItem as="a" href="https://exchange.pancakeswap.finance/">
+        <ButtonMenuItem as="a" href="https://teckgeekz-exch.netlify.app/">
           V2
         </ButtonMenuItem>
-        <ButtonMenuItem as="a" href="https://v1exchange.pancakeswap.finance/#/">
+        <ButtonMenuItem as="a" href="https://teckgeekz-exch.netlify.app/#/">
           V1 (old)
         </ButtonMenuItem>
       </ButtonMenu>
       <Button
         variant="subtle"
         as={Link}
-        href="https://v1exchange.pancakeswap.finance/#/migrate"
+        href="https://teckgeekz-exch.netlify.app/#/migrate"
         endIcon={<HelpIcon color="white" />}
         scale="sm"
         ml="16px"
